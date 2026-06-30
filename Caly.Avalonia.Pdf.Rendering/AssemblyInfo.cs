e1200590ae7edaf7d845458a905bda75176bfb4e
BobLd <38405645+BobLd@users.noreply.github.com>
2026-06-30 10:42:34 +0100

    feat(rendering): scaffold Caly.Avalonia.Pdf.Rendering package

---
+++ Caly.Avalonia.Pdf.Rendering/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 BobLd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Caly.Avalonia.Pdf.Rendering.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100512e743ab1639311b6c83dc543bb720507ee89716b651cc1d4c527260431b5b22f19ec8a5e91d6078c9fd0e5074dd0d6997e523f9639d2efbcb16927715aecba619b597e7a945e369ca9d8ab569d91ea0a17b21184733e871b90fc3029ec705bc55b33f7edef03090c41c0a576e3e45744d160f162c5896593ab7845f485f7a5")]
